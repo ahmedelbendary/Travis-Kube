bd3bf630b02fdf6126b8a069544d49c98fbbde69
ahmed Elbendary <ahmed.elbendary@ocucon.com>
2021-06-13 15:30:26 +0200

    second

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, From AhmedElbendary/Travis account !!\n This is my first Task in Ocucon'  
+    return 'Hello, From AhmedElbendary/Travis account !!\n Deploy on kubernetes cluster'  
