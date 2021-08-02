df46375bb5811511e0eeb68402017a98bd0df34b
ahmed Elbendary <ahmed.elbendary@ocucon.com>
2021-08-02 10:45:22 +0200

    record-prod

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, From AhmedElbendary/Travis account !!\n R4.0.0-prod  Deploy on kubernetes cluster prod namespace'  
+    return 'Hello, From AhmedElbendary/Travis account !!\n R1.1.1-prod  Deploy on kubernetes cluster prod namespace'  
